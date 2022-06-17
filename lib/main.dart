1c906fb9c2ad604fa389d3a03d7125b83530b7e8
GabrielFdoVal <gabrielfdoval1@gmail.com>
2022-06-17 12:44:00 -0300

    Initial commit first component and screen
    On main.dart added themes ofr fonts and colors, and the home screen is categories_screen.dart.
    categories_screen.dart have a grid layout that contains a category_item.dart component;
    this component stylizes the itens in the dummy list that use the category.dart as model.
    
    the fonts are defined in the pubspec.yaml.

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'screens/categories_screen.dart';
+
+void main() {
+  runApp(MyApp());
+  print("a");
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primaryColor: Colors.pink,
+        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.amber),
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
